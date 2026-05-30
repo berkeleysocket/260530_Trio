822b46a2b4f69f0167855f05782e72c0e5e6758b
hwani165 <seungyung.kang@gmail.com>
2026-05-30 16:20:37 +0900

    feat : Session

--- Assets/_Scripts/Runtime/Server/ServerBootstrap.cs
+++ Assets/_Scripts/Runtime/Server/ServerBootstrap.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using System.Net.Sockets;
 
 public class ServerBootstrap
 {
     private Socket _socket;
+    private List<Session> _clients;
+
+    private SocketAsyncEventArgs _acceptArgs;
 
     public void Initialize()
     {
@@ -10,5 +14,22 @@ public class ServerBootstrap
         SocketType socketType = SocketType.Stream;
         ProtocolType protocol = ProtocolType.Tcp;
         _socket = new Socket(addressFamily, socketType, protocol);
+
+        _socket.Listen(10);
+
+        _acceptArgs = new SocketAsyncEventArgs();
+        _acceptArgs.Completed += HandleAccept;
+        bool pending = _socket.AcceptAsync(_acceptArgs);
+        if (!pending)
+            HandleAccept(null, _acceptArgs);
+    }
+
+    private void HandleAccept(object sender, SocketAsyncEventArgs args)
+    {
+        _socket.AcceptAsync(_acceptArgs);
+        Session client = new Session(args.AcceptSocket);
+        _clients.Add(client);
+
+        client.Receive();
     }
 }
---
+++ Assets/_Scripts/Runtime/Shared/Session.cs
@@ -0,0 +1,27 @@
+using System.Net.Sockets;
+
+public class Session
+{
+    private Socket _connectedSocket;
+    private SocketAsyncEventArgs _receiveArgs;
+
+    public Session(Socket connectedSocket)
+    {
+        this._connectedSocket = connectedSocket;
+
+        _receiveArgs = new SocketAsyncEventArgs();
+        _receiveArgs.Completed += HandleReceive;
+    }
+
+    public void Receive()
+    {
+        bool pending = _connectedSocket.ReceiveAsync(_receiveArgs);
+        if (!pending)
+            HandleReceive(null, _receiveArgs);
+    }
+
+    private void HandleReceive(object sender, SocketAsyncEventArgs args)
+    {
+
+    }
+}
