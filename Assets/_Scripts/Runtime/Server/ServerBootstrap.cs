fdc8f70ee48a8e240bb664f73afd789db7b5e09e
hwani165 <seungyung.kang@gmail.com>
2026-06-01 18:27:44 +0900

    feat : CustomLog.cs

--- Assets/_Scripts/Runtime/Server/ServerBootstrap.cs
+++ Assets/_Scripts/Runtime/Server/ServerBootstrap.cs
@@ -1,41 +1,11 @@
-using System.Collections.Generic;
-using System.Net.Sockets;
+using UnityEngine;
 
-public class ServerBootstrap
+public class ServerBootstrap : MonoBehaviour
 {
-    private Socket _socket;
-    private List<Session> _clients;
-    private SocketAsyncEventArgs _acceptArgs;
+    private Server _server;
 
-    public void Initialize()
+    private void Awake()
     {
-        AddressFamily addressFamily = AddressFamily.InterNetwork;
-        SocketType socketType = SocketType.Stream;
-        ProtocolType protocol = ProtocolType.Tcp;
-        _socket = new Socket(addressFamily, socketType, protocol);
-
-        _acceptArgs = new SocketAsyncEventArgs();
-        _acceptArgs.Completed += HandleAccept;
-    }
-
-    public void Listen()
-    {
-        _socket.Listen(10);
-    }
-
-    public void Accept()
-    {
-        bool pending = _socket.AcceptAsync(_acceptArgs);
-        if (!pending)
-            HandleAccept(null, _acceptArgs);
-    }
-
-    private void HandleAccept(object sender, SocketAsyncEventArgs args)
-    {
-        _socket.AcceptAsync(_acceptArgs);
-        Session client = new Session(args.AcceptSocket);
-        _clients.Add(client);
-
-        client.Receive();
+        _server = new Server();    
     }
 }
--- Assets/_Scripts/Runtime/Shared/Session.cs
+++ Assets/_Scripts/Runtime/Shared/Session.cs
@@ -22,6 +22,6 @@ public class Session
 
     private void HandleReceive(object sender, SocketAsyncEventArgs args)
     {
-
+        args.
     }
 }
