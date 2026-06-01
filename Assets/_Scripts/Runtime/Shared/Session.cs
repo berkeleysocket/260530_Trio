e569f13e12f5458ba4e6d9db1925ea1ff8b496ac
hwani165 <seungyung.kang@gmail.com>
2026-06-01 20:00:33 +0900

    Update Session.cs

--- Assets/_Scripts/Runtime/Shared/Session.cs
+++ Assets/_Scripts/Runtime/Shared/Session.cs
@@ -1,9 +1,12 @@
+using KSY.Utility;
+using System;
 using System.Net.Sockets;
 
 public class Session
 {
     private Socket _connectedSocket;
     private SocketAsyncEventArgs _receiveArgs;
+    private ReceiveBuffer _receiveBuffer;
 
     public Session(Socket connectedSocket)
     {
@@ -11,6 +14,8 @@ public class Session
 
         _receiveArgs = new SocketAsyncEventArgs();
         _receiveArgs.Completed += HandleReceive;
+
+        _receiveBuffer = new ReceiveBuffer(4096);
     }
 
     public void Receive()
@@ -20,8 +25,26 @@ public class Session
             HandleReceive(null, _receiveArgs);
     }
 
+    public void Send()
+    {
+
+    }
+
     private void HandleReceive(object sender, SocketAsyncEventArgs args)
     {
-        
+        if(args.SocketError == SocketError.Success
+            && args.BytesTransferred > 0)
+        {
+            int bytesTransferred = args.BytesTransferred;
+            byte[] buffer = args.Buffer;
+            ArraySegment<byte> segment = _receiveBuffer.WriteSegment();
+            Array.Copy(buffer, 0, segment.Array, segment.Offset, bytesTransferred);
+            _receiveBuffer.OnWrite(bytesTransferred);
+        }
+        else
+        {
+            CustomLog.LogError(args.SocketError.ToString());
+        }
     }
+
 }
